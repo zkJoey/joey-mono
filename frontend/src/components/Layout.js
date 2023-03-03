055ea3646b7f18865d61394b709d2ee99fb026db
Ekrem BAL <mail.ekrembal@gmail.com>
2023-03-02 22:06:11 -0700

    Better state managment

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, creditWalletAddress }) => {
   return (
     <>
-      {/* <Header />
-      <main>{children}</main>
-      <Footer /> */}
-      <main>{children}</main>
+      <Header creditWalletAddress={creditWalletAddress} />
+      <div className="max-w-screen-xl ">{children}</div>
       <Footer />
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,9 @@ import { ethers } from 'ethers';
 
 // pages
 import Home from './pages/Home';
-import DeployAA from './pages/DeployAA';
+import DeployCreditWallet from './pages/DeployCreditWallet';
 import CreditLine from './pages/CreditLine';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// components
-import MyWalletButton from './components/MyWalletButton'
 
 // Web3Modal
 import {
@@ -26,7 +23,7 @@ import { Web3Button, Web3Modal, Web3NetworkSwitch } from '@web3modal/react'
 
 import { Link, useLocation } from 'react-router-dom';
 
-// import {getSmartContractWalletAddress} from './utils/utils'
+// import {getCreditWalletAddress} from './utils/utils'
 
 
 // 1. Get projectID at https://cloud.walletconnect.com
@@ -71,20 +68,11 @@ const wagmiClient = createClient({
 const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 
-const routes = {
-  'Home': '/',
-  'Credit Line': '/credit-line',
-  'Exchange': '/exchange',
-}
-
-const Header = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
-  const [smartContractWalletAddress, setSmartContractWalletAddress] = useState(null);
-  const location = useLocation();
-
+const App = () => {
+  const [creditWalletAddress , setCreditWalletAddress] = useState(null);
   useEffect(() => {
     
-    const getSmartContractWalletAddress = async () => {
+    const getCreditWalletAddress = async () => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
@@ -110,103 +98,32 @@ const Header = () => {
         }
       ];
       const contract = new ethers.Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", abi, signer);
-      const smartContractWalletAddress = await contract.wallets(address);
-      console.log("SASD", smartContractWalletAddress);
-      if(smartContractWalletAddress != 0x00)
-        setSmartContractWalletAddress(smartContractWalletAddress);
+      const creditWalletAddress = await contract.wallets(address);
+      console.log("SASD", creditWalletAddress);
+      if(creditWalletAddress != 0x00)
+        setCreditWalletAddress(creditWalletAddress);
     }
-    getSmartContractWalletAddress();
+    getCreditWalletAddress();
   }, []);
 
-  const NavItem = ({ name, route }) => {
-    return (<li>
-      <Link
-        to={route}
-        className={
-          location.pathname === {route}
-            ? 'block py-2 pr-4 pl-3 md:p-0'
-            : 'block py-2 pr-4 pl-3 text-gray-400 md:border-0 md:p-0'
-        }
-        onClick={() => {
-          setIsNavExpanded(false);
-        }}
-      >
-        {name}
-      </Link>
-    </li>);
-    };
-
   return (
-    <header className={isNavExpanded ? 'menu-open pb-40' : 'pb-40'}>
-      <nav className="border-gray-200 px-2 sm:px-4 py-2.5 primary-menu left-0 rigt-0 z-20 fixed w-full bg-white border-b">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <Link
-            to="/"
-            className="flex items-center"
-            onClick={() => {
-              setIsNavExpanded(false);
-            }}
-          >
-            {/* <img src="https://chainway.xyz/img/favicon.jpg" alt="Boilerplate" width="30" height="60" className="mr-4" /> */}
-            <span className="self-center text-xl font-bold whitespace-nowrap">
-            Joey Finance
-            </span>
-          </Link>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-            onClick={() => {
-              setIsNavExpanded(!isNavExpanded);
-            }}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg className="svg-trigger" viewBox="0 0 100 100">
-              <path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path>
-              <path d="m 30,50 h 40"></path>
-              <path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path>
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 items-center">
-              {Object.keys(routes).map((name) => (
-                <NavItem key={name} name={name} route={routes[name]} />
-              ))}
-              <li>
-              <Web3Button />
-              <Web3NetworkSwitch />
-              {smartContractWalletAddress && <MyWalletButton address={smartContractWalletAddress} />}
-              
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <WagmiConfig client={wagmiClient}>
+    <React.StrictMode>
+    <Router>
+      <Layout creditWalletAddress={creditWalletAddress}>
+        <Routes>
+          <Route path="/" element={<DeployCreditWallet setCreditWalletAddress={setCreditWalletAddress}/>}></Route>
+          <Route path="/credit-line" element={<CreditLine />}></Route>
+        </Routes>
+        <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      </Layout>
+    </Router>
+  </React.StrictMode>
+  </WagmiConfig>
   );
-};
-
-
+}  
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <WagmiConfig client={wagmiClient}>
-  <React.StrictMode>
-  <Router>
-    <Layout>
-
-      <Header />
-      <Routes>
-        <Route path="/" element={<DeployAA />}></Route>
-        <Route path="/credit-line" element={<CreditLine />}></Route>
-      </Routes>
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </Layout>
-  </Router>
-</React.StrictMode>
-</WagmiConfig>
-);
+root.render(<App />);
